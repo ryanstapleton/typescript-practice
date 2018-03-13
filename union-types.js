b018299ac1f420c9ebc3dafa5c7e0fa8ab8c8ccd
ryanstapleton <megalixir.1@gmail.com>
2018-03-13 10:18:54 -0600

    Additional TS practice files added

---
+++ union-types.js
@@ -0,0 +1,9 @@
+var players = ["Altuve", "Corerra", "Bregman"];
+var player_numbers = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+var names;
+names = ["Ryan", "Someone else"];
+console.log(names);
+names = "Someone Else";
+console.log(names);
